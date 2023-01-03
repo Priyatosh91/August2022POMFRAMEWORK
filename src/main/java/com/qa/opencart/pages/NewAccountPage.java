1d02a3ee76309d7ed82a37694f573c7feabfa6ba
Smru_pru <smru_pru@Smru-prus-MacBook-Pro.local>
2023-01-03 21:59:41 +0530

    added new account page

---
+++ src/main/java/com/qa/opencart/pages/NewAccountPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class NewAccountPage {
+	
+	private By emailId = By.id("input-email");
+	private By password = By.id("input-password");
+	private By loginBtn = By.xpath("//input[@value='Login']");
+	private By forgotPwdLink = By.linkText("Forgotten Password");
+
+}
