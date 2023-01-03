252926904cd05ceb2d68b978025386df56abe2f1
Smru_pru <smru_pru@Smru-prus-MacBook-Pro.local>
2023-01-03 22:05:03 +0530

    modified variable name

--- src/main/java/com/qa/opencart/pages/NewAccountPage.java
+++ src/main/java/com/qa/opencart/pages/NewAccountPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 
 public class NewAccountPage {
 	
-	private By emailId = By.id("input-email");
+	private By emailId_new = By.id("input-email");
 	private By password = By.id("input-password");
 	private By loginBtn = By.xpath("//input[@value='Login']");
 	private By forgotPwdLink = By.linkText("Forgotten Password");
