8ff553de9fd3ce207b417ee6f918e039a89f9ec5
Smru_pru <smru_pru@Smru-prus-MacBook-Pro.local>
2023-01-07 18:31:55 +0530

    added new Account page

---
+++ src/main/java/com/qa/opencart/pages/NewAcccountPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class NewAcccountPage {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is my new account page");
+		
+		
+		
+	}
+
+}
