584b099c85f448a28dcfeb0dc2bd988fed2a303d
Smru_pru <smru_pru@Smru-prus-MacBook-Pro.local>
2023-01-07 18:53:52 +0530

    added changes

--- src/main/java/com/qa/opencart/pages/NewAcccountPage.java
+++ src/main/java/com/qa/opencart/pages/NewAcccountPage.java
@@ -6,9 +6,9 @@ public class NewAcccountPage {
 		
 		System.out.println("This is my new account page");
 		
-		int z =10;
-		int z1 =20;
-		System.out.println(z+z1);
+		int z1 =10;
+		int z2 =20;
+		System.out.println(z1+z2);
 		
 	}
 
