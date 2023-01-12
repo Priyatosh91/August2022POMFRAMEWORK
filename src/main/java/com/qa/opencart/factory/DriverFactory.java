5b9f03042966a39e595b684ed965511d9fce5f72
Smru_pru <smru_pru@Smru-prus-MacBook-Pro.local>
2023-01-12 09:23:11 +0530

    Added and deleted files

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -28,15 +28,15 @@ public class DriverFactory {
 
 	public WebDriver init_driver(Properties prop) {
 
-	//	String browserName = prop.getProperty("browser").trim();
+		String browserName = prop.getProperty("browser").trim();
 
-	//	System.out.println("Browser name is : " + browserName);
+		System.out.println("Browser name is : " + browserName);
 
 		// mvn clean install -Denv="qa" -Dbrowser="chrome"
 
-		 String browserName = System.getProperty("browser");
+	//	 String browserName = System.getProperty("browser");
 		 
-		 System.out.println("Browser name is : " + browserName);
+	//	 System.out.println("Browser name is : " + browserName);
 
 		// System.out.println("Browser passed from command line: " + browser);
 
